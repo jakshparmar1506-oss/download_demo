ce77920a88710fb7b8f6507ed45d45f668b61e79
baldev parmar <jaksh.parmar1506@gmail.com>
2026-01-12 22:17:45 +0530

    ddfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
 "use client";
 
 export default function Home() {
-  const downloadTxt = (content: string, fileName = "file.txt") => {
-    const blob = new Blob([content], {
-      type: "text/plain;charset=utf-8",
-    });
+const downloadTxtIOS = (content: string, fileName = "file.txt") => {
+  const blob = new Blob([content], {
+    type: "application/octet-stream", // 👈 IMPORTANT
+  });
 
-    const url = URL.createObjectURL(blob);
+  const url = URL.createObjectURL(blob);
 
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = fileName;
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = fileName;
 
-    document.body.appendChild(a);
-    a.click();
+  document.body.appendChild(a);
+  a.click();
 
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  };
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+};
 
-  return (
-    <div>
-      <button
-        onClick={() =>
-          downloadTxt("Hello from UI\nWorks on iOS & Android", "ui-file.txt")
-        }
-      >
-        Download TXT
-      </button>
-    </div>
-  );
+return (
+  <div>
+    <button
+      onClick={() =>
+        downloadTxtIOS("Hello from UI\nWorks on iOS & Android", "ui-file.txt")
+      }
+    >
+      Download TXT
+    </button>
+  </div>
+);
 }
